46f64645f7f2715ddaf880587324bdb97c474f11
VaiTon <eyadlorenzo@gmail.com>
2024-07-31 16:42:22 +0200

    Fix nullable var in folksonomy

--- src/routes/folksonomy/[key]/+page.ts
+++ src/routes/folksonomy/[key]/+page.ts
@@ -6,7 +6,7 @@ export const ssr = false;
 
 export const load: PageLoad = async ({ fetch, params }) => {
 	const { key } = params;
-	const tags = await new FolksonomyApi(fetch).getProducts(key);
+	const tags = (await new FolksonomyApi(fetch).getProducts(key)) ?? [];
 
 	const productsApi = new ProductsApi(fetch);
 	const products = Promise.all(tags.map((tag) => productsApi.getProductName(tag.product)));
