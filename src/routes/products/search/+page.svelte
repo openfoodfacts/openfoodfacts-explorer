6d5f6293d0955a2588d1bf9d674b9e5a0d97a624
Abbas Askari <52373866+Abbas-Askari@users.noreply.github.com>
2025-03-01 20:08:50 +0500

    enhancement: Add no products found info on search page (#162)

--- src/routes/products/search/+page.svelte
+++ src/routes/products/search/+page.svelte
@@ -20,36 +20,43 @@
 		<div class="skeleton dark:bg-base-300 h-24 bg-white p-4 shadow-md"></div>
 	{/each}
 {:then result}
-	{#each result.products as product}
-		<SmallProductCard {product} />
-	{/each}
+	{#if result.count > 0}
+		{#each result.products as product}
+			<SmallProductCard {product} />
+		{/each}
 
-	<div class="join my-8 justify-center">
-		{#if result.page > 1}
-			<a href={getPageUrl($page.url, 1)} class="btn join-item"> 1 </a>
-		{/if}
-		{#if result.page > 3}
-			<button class="btn btn-disabled join-item">...</button>
-		{/if}
+		<div class="join my-8 justify-center">
+			{#if result.page > 1}
+				<a href={getPageUrl($page.url, 1)} class="btn join-item"> 1 </a>
+			{/if}
+			{#if result.page > 3}
+				<button class="btn btn-disabled join-item">...</button>
+			{/if}
 
-		{#if result.page > 2}
-			<a href={getPageUrl($page.url, result.page - 1)} class="btn join-item">
-				{result.page - 1}
-			</a>
-		{/if}
+			{#if result.page > 2}
+				<a href={getPageUrl($page.url, result.page - 1)} class="btn join-item">
+					{result.page - 1}
+				</a>
+			{/if}
 
-		<button class="btn join-item btn-active">{result.page}</button>
+			<button class="btn join-item btn-active">{result.page}</button>
 
-		{#if result.page_count > result.page + 1}
-			<a href={getPageUrl($page.url, result.page + 1)} class="btn join-item">{result.page + 1}</a>
-		{/if}
-		{#if result.page_count > result.page + 2}
-			<button class="btn btn-disabled join-item">...</button>
-		{/if}
-		{#if result.page_count > result.page}
-			<a href={getPageUrl($page.url, result.page_count)} class="btn join-item">
-				{result.page_count}
-			</a>
-		{/if}
-	</div>
+			{#if result.page_count > result.page + 1}
+				<a href={getPageUrl($page.url, result.page + 1)} class="btn join-item">{result.page + 1}</a>
+			{/if}
+			{#if result.page_count > result.page + 2}
+				<button class="btn btn-disabled join-item">...</button>
+			{/if}
+			{#if result.page_count > result.page}
+				<a href={getPageUrl($page.url, result.page_count)} class="btn join-item">
+					{result.page_count}
+				</a>
+			{/if}
+		</div>
+	{:else}
+		<div class="mt-8 text-center opacity-70">
+			<p>No products found</p>
+			<p>We couldn't find any products matching your search</p>
+		</div>
+	{/if}
 {/await}
