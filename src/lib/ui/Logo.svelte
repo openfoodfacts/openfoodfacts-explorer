a18ee3d8f04c53a5086f41fc89812e3f2c8d0f23
Jiya Gupta <coderaholic21@gmail.com>
2025-03-05 03:53:00 +0530

    update logo according to the theme

--- src/lib/ui/Logo.svelte
+++ src/lib/ui/Logo.svelte
@@ -1,10 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+	import settings from '$lib/settings';
+
+	let imageUrl = 'https://static.openfoodfacts.org/images/logos/off-logo-horizontal-dark.svg';
+
+	onMount(() => {
+		settings.subscribe(($settings) => {
+			imageUrl =
+				$settings.theme === 'dark'
+					? 'https://static.openfoodfacts.org/images/logos/off-logo-horizontal-dark.svg'
+					: 'https://static.openfoodfacts.org/images/logos/off-logo-horizontal-light.svg';
+		});
+	});
+</script>
+
 <picture>
-	<source
-		srcset="https://static.openfoodfacts.org/images/logos/off-logo-horizontal-dark.svg"
-		media="(prefers-color-scheme: dark)"
-	/>
-	<img
-		src="https://static.openfoodfacts.org/images/logos/off-logo-horizontal-light.svg"
-		alt="OpenFoodFacts Explorer"
-	/>
+	<img src={imageUrl} alt="OpenFoodFacts Explorer" />
 </picture>
