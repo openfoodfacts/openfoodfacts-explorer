d29c79df7fd9506f1fdf606147ca7106cf90e088
Rishik <86944435+RISHIK92@users.noreply.github.com>
2025-03-09 02:07:39 +0530

    Update +page.svelte and fixed requested changes

--- src/routes/qr/+page.svelte
+++ src/routes/qr/+page.svelte
@@ -2,7 +2,7 @@
 	import { onDestroy, onMount } from 'svelte';
 	import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 
-	let error: string | null = null;
+	let error: string | null = $state(null);
 	let html5QrCode: Html5Qrcode | null = null;
 
 	onMount(() => {
@@ -52,7 +52,7 @@
 	});
 </script>
 
-{#if error}
+{#if error!=null}
 	<div class="flex items-center justify-center h-screen">
 		<p class="text-red-500">{error}</p>
 	</div>
