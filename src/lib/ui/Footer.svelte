f8b4dfa7cca34b3a9cf86bd1dfe1fb6fe07060a9
VaiTon <eyadlorenzo@gmail.com>
2025-03-11 20:42:28 +0100

    style: use theme colors in footer

--- src/lib/ui/Footer.svelte
+++ src/lib/ui/Footer.svelte
@@ -1,31 +1,36 @@
 <script lang="ts">
+	const projectLinks = [
+		{ url: 'https://world.openfoodfacts.org/who-we-are', text: 'Who we are' },
+		{
+			url: 'https://world.openfoodfacts.org/open-food-facts-vision-mission-values-and-programs',
+			text: 'Vision, Mission, Values and Programs'
+		},
+		{ url: 'https://support.openfoodfacts.org/help/en-gb', text: 'Frequently asked questions' },
+		{ url: 'https://blog.openfoodfacts.org/en/', text: 'Open Food Facts blog' },
+		{ url: 'https://world.openfoodfacts.org/press', text: 'Press' },
+		{ url: 'https://wiki.openfoodfacts.org/', text: 'Open Food Facts wiki (en)' },
+		{ url: 'https://world.openfoodfacts.org/cgi/top_translators.pl', text: 'Translators' },
+		{ url: 'https://world.openfoodfacts.org/partners', text: 'Partners' },
+		{ url: 'https://world.openbeautyfacts.org/', text: 'Open Food Facts - Cosmetics' },
+		{ url: 'https://world.pro.openfoodfacts.org/', text: 'Open Food Facts for Producers' }
+	];
 
-    const projectLinks = [
-        { url: 'https://world.openfoodfacts.org/who-we-are', text: 'Who we are' }, 
-        { url: 'https://world.openfoodfacts.org/open-food-facts-vision-mission-values-and-programs', text: 'Vision, Mission, Values and Programs' }, 
-        { url: 'https://support.openfoodfacts.org/help/en-gb', text: 'Frequently asked questions' }, 
-        { url: 'https://blog.openfoodfacts.org/en/', text: 'Open Food Facts blog' }, 
-        { url: 'https://world.openfoodfacts.org/press', text: 'Press' }, 
-        { url: 'https://wiki.openfoodfacts.org/', text: 'Open Food Facts wiki (en)' }, 
-        { url: 'https://world.openfoodfacts.org/cgi/top_translators.pl', text: 'Translators' }, 
-        { url: 'https://world.openfoodfacts.org/partners', text: 'Partners' }, 
-        { url: 'https://world.openbeautyfacts.org/', text: 'Open Food Facts - Cosmetics' }, 
-        { url: 'https://world.pro.openfoodfacts.org/', text: 'Open Food Facts for Producers' }
-    ]
-
-    const footerLinks = [
-        { url: 'https://world.openfoodfacts.org/legal', text: 'Legal' }, 
-        { url: 'https://world.openfoodfacts.org/privacy', text: 'Privacy' }, 
-        { url: 'https://world.openfoodfacts.org/terms-of-use', text: 'Terms of use' }, 
-        { url: 'https://world.openfoodfacts.org/data', text: 'Data, API and SDKs' }, 
-        { url: 'https://world.openfoodfacts.org/donate-to-open-food-facts', text: 'Donate to Open Food Facts' }, 
-        { url: 'https://world.pro.openfoodfacts.org/', text: 'Producers' }, 
-        { url: 'https://link.openfoodfacts.org/newsletter-en', text: 'Subscribe to our newsletter' }
-    ]
+	const footerLinks = [
+		{ url: 'https://world.openfoodfacts.org/legal', text: 'Legal' },
+		{ url: 'https://world.openfoodfacts.org/privacy', text: 'Privacy' },
+		{ url: 'https://world.openfoodfacts.org/terms-of-use', text: 'Terms of use' },
+		{ url: 'https://world.openfoodfacts.org/data', text: 'Data, API and SDKs' },
+		{
+			url: 'https://world.openfoodfacts.org/donate-to-open-food-facts',
+			text: 'Donate to Open Food Facts'
+		},
+		{ url: 'https://world.pro.openfoodfacts.org/', text: 'Producers' },
+		{ url: 'https://link.openfoodfacts.org/newsletter-en', text: 'Subscribe to our newsletter' }
+	];
 </script>
 
 <div
-	class="bg-secondary mt-10 flex flex-col justify-between gap-5 px-10 py-8 text-white dark:text-black md:flex-row md:px-20 lg:px-40"
+	class="bg-secondary text-secondary-content mt-10 flex flex-col justify-between gap-5 px-10 py-8 md:flex-row md:px-20 lg:px-40"
 >
 	<div class="flex flex-col gap-1">
 		<div class="text-lg font-bold">Join the community</div>
@@ -76,36 +81,36 @@
 	<div class="flex flex-col gap-2 md:max-w-1/2">
 		<div class="text-lg font-bold">Discover the project</div>
 		<div class="flex flex-wrap gap-3 text-sm">
-			{#each projectLinks as link}
+			{#each projectLinks as link (link.url)}
 				<a
 					href={link.url}
-					class="hover:bg-base-300 rounded-lg bg-white px-2 py-1 text-black transition-all duration-300 hover:text-white"
-					>{link.text}</a
+					class="bg-base-300 text-base-content hover:bg-primary hover:text-primary-content rounded-lg px-2 py-1 transition-all duration-300"
 				>
+					{link.text}
+				</a>
 			{/each}
 		</div>
 	</div>
 </div>
 
-<div
-	class="bg-base-200 manrope flex flex-col items-center justify-center gap-3 px-6 py-5 text-black dark:text-white"
->
-	<div>
-		<a href="/">
-			<img
-				src="https://static.openfoodfacts.org/images/logos/off-logo-horizontal-mono-white.svg"
-				alt="Open Food Facts"
-			/>
-		</a>
-	</div>
+<div class="bg-base-200 flex flex-col items-center justify-center gap-3 px-6 py-5">
+	<picture>
+		<source
+			srcset="https://static.openfoodfacts.org/images/logos/off-logo-horizontal-mono-white.svg"
+			media="(prefers-color-scheme: dark)"
+		/>
+		<img
+			src="https://static.openfoodfacts.org/images/logos/off-logo-horizontal-mono-black.svg"
+			alt="Open Food Facts"
+		/>
+	</picture>
+
 	<div class="text-sm">
 		A collaborative, free and open database of food products from around the world.
 	</div>
-	<div class="textarea-xs">
-		<div class="flex flex-wrap gap-3">
-			{#each footerLinks as footerLink}
-				<a href={footerLink.url} class="underline">{footerLink.text}</a>
-			{/each}
-		</div>
+	<div class="flex w-full flex-wrap justify-center gap-3 text-xs">
+		{#each footerLinks as footerLink (footerLink.url)}
+			<a href={footerLink.url} class="underline">{footerLink.text}</a>
+		{/each}
 	</div>
 </div>
