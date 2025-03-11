a5db378553d23291d7efa68d77b3d08d3d51bba6
Bhavishya Garg <147748752+Bhavishya2601@users.noreply.github.com>
2025-03-12 01:14:19 +0530

    feat: add global footer (#261)
    * style: use theme colors in footer
    * fix: do not repeat key in #each block
    
    ---------
    
    Co-authored-by: VaiTon <eyadlorenzo@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,11 +39,11 @@
 	<div class="mt-8 w-full">
 		<div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
 			{#await data.streamed.products}
-				{#each Array(4) as i (i)}
+				{#each [...Array(4).keys()] as i (i)}
 					<div class="skeleton dark:bg-base-300 h-28 bg-white p-4 shadow-md"></div>
 				{/each}
 			{:then products}
-				{#each products as state}
+				{#each products as state (state.product.code)}
 					<SmallProductCard product={state.product} />
 				{/each}
 			{/await}
