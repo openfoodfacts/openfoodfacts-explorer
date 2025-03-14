0717cc805fa6992828d4a3b6d65129384514dcf4
bhavishya2601 <bgarg2050bhav@gmail.com>
2025-03-15 00:01:44 +0530

    preference section on product page

--- src/routes/products/[barcode]/+page.svelte
+++ src/routes/products/[barcode]/+page.svelte
@@ -149,7 +149,7 @@
 	</div>
 </Card>
 
-<div class="flex w-full justify-evenly gap-4 p-3">
+<div class="flex w-full justify-between gap-3 max-h-32">
 	<NutriScore grade={product.nutriscore_grade} />
 	<Nova grade={product.nova_group} />
 	<a href="#environment_card">
