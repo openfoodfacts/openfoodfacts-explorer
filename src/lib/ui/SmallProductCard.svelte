99191805705bae865d149e27c3520e359f7393c0
shivam shukla <shivamshukla8112005@gmail.com>
2025-03-22 16:17:06 +0530

    convert card component layout from vertical to horizontal

--- src/lib/ui/SmallProductCard.svelte
+++ src/lib/ui/SmallProductCard.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { ProductReduced } from '$lib/api';
-	import { navigating } from '$app/state';
+	import { navigating } from '$app/stores';
 
 	interface Props {
 		product: ProductReduced;
@@ -11,35 +11,36 @@
 
 <a
 	href={`/products/${product.code}`}
-	class="btn btn-ghost text-primary dark:bg-base-300 pointer-events-none m-auto flex h-60 w-56 flex-col items-center justify-normal space-y-2 rounded-2xl bg-white p-4 text-center shadow-md"
-	class:pointer-events-none={navigating.to}
+	class="btn btn-ghost text-primary dark:bg-base-300 pointer-events-none h-[133.6px] justify-normal rounded-2xl bg-white p-4 text-start shadow-md"
+	class:pointer-events-none={$navigating}
 >
-	<!-- Image Section -->
-	<div class="flex h-24 w-24 items-center justify-center overflow-hidden">
-		{#if navigating.to?.params?.barcode === product.code}
-			<span class="loading loading-ring loading-lg mx-auto my-auto"></span>
-		{/if}
-		{#if product.image_front_small_url != null}
-			<img
-				src={product.image_front_small_url}
-				alt={product.product_name}
-				class="h-full w-full object-contain"
-			/>
-		{:else}
-			<div
-				class="placeholder flex aspect-square h-full w-full items-center justify-center bg-amber-50 text-black"
+	<div class="flex flex-row items-center overflow-hidden">
+		<div class="mr-4 flex w-16 shrink-0 items-center justify-center">
+			{#if $navigating?.to?.params?.barcode === product.code}
+				<span class="loading loading-ring loading-lg mx-auto my-auto"></span>
+			{:else if product.image_front_small_url}
+				<img
+					src={product.image_front_small_url}
+					class="h-16 rounded-lg object-cover"
+					alt="Product front"
+				/>
+			{:else}
+				<div
+					class="placeholder flex aspect-square h-full w-full items-center justify-center bg-amber-50 text-black"
+				>
+					No Image
+				</div>
+			{/if}
+		</div>
+		<div class="m-auto flex w-full flex-col items-center overflow-hidden text-ellipsis">
+			<p class="line-clamp-2 w-full overflow-hidden text-center text-lg text-ellipsis">
+				{product.product_name ?? product.code}
+			</p>
+			<p
+				class="mt-1 line-clamp-2 w-full overflow-hidden text-center text-sm font-light text-ellipsis"
 			>
-				No Image
-			</div>
-		{/if}
-	</div>
-
-	<div class="m-auto flex w-full flex-col items-center">
-		<p class="line-clamp-2 w-full text-center text-lg">
-			{product.product_name ?? product.code}
-		</p>
-		<p class="mt-1 line-clamp-2 w-full text-center text-sm font-light">
-			{product.brands} - {product.quantity}
-		</p>
+				{product.brands} - {product.quantity}
+			</p>
+		</div>
 	</div>
 </a>
