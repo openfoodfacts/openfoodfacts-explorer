e301350d83723ddca2e746399f928e6e50e0311e
heymitali <mitalisinghnaruka@gmail.com>
2025-03-23 22:55:02 +0530

    improved ui of product cards

--- src/lib/ui/SmallProductCard.svelte
+++ src/lib/ui/SmallProductCard.svelte
@@ -7,30 +7,34 @@
 	}
 
 	let { product }: Props = $props();
+
+	function capitalizeWords(name: string): string {
+		return name.replace(/\b\w/g, char => char.toUpperCase());
+	}
 </script>
 
 <a
 	href={`/products/${product.code}`}
-	class="btn btn-ghost text-primary dark:bg-base-300 pointer-events-none h-auto justify-normal rounded-2xl bg-white p-4 text-start shadow-md"
+	class="block"
 	class:pointer-events-none={navigating.to}
 >
-	<div class="flex flex-row items-center">
-		<div class="mr-4 flex w-16 shrink-0 items-center justify-center">
+	<div class="h-[14rem] w-56 bg-white shadow-md rounded-2xl">
+		<div class="h-[10rem] w-full text-center text-sm">
 			{#if navigating.to?.params?.barcode === product.code}
 				<span class="loading loading-ring loading-lg mx-auto my-auto"></span>
 			{:else if product.image_front_small_url}
 				<img
-					src={product.image_front_small_url}
-					class="h-16 rounded-lg object-cover"
+					src={product.image_front_small_url ?? ''}
+					class="w-full h-full object-cover overflow-hidden rounded-t-2xl"
 					alt="Product front"
 				/>
 			{/if}
 		</div>
-		<div>
-			<p class="text-lg">
-				{product.product_name ?? product.code}
-			</p>
-			<p class="mt-2 text-sm font-light">
+		<div class="flex flex-col justify-center items-center font-semibold text-black px-2 pt-2">
+			<div class="font-bold text-center truncate w-[100%] 2xl:text-[18px]" title={product.product_name ? capitalizeWords(product.product_name) : product.code}>
+				{product.product_name ? capitalizeWords(product.product_name) : product.code}
+			</div>
+			<p class="text-xs">
 				{product.brands} - {product.quantity}
 			</p>
 		</div>
