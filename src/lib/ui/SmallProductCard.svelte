af35abea6f2e6222936372de33e89f28f9e8044b
heymitali <mitalisinghnaruka@gmail.com>
2025-03-23 23:10:29 +0530

    updated text sizes

--- src/lib/ui/SmallProductCard.svelte
+++ src/lib/ui/SmallProductCard.svelte
@@ -9,32 +9,31 @@
 	let { product }: Props = $props();
 
 	function capitalizeWords(name: string): string {
-		return name.replace(/\b\w/g, char => char.toUpperCase());
+		return name.replace(/\b\w/g, (char) => char.toUpperCase());
 	}
 </script>
 
-<a
-	href={`/products/${product.code}`}
-	class="block"
-	class:pointer-events-none={navigating.to}
->
-	<div class="h-[14rem] w-56 bg-white shadow-md rounded-2xl">
-		<div class="h-[10rem] w-full text-center text-sm">
+<a href={`/products/${product.code}`} class="block" class:pointer-events-none={navigating.to}>
+	<div class="h-[11.5rem] w-[16rem] rounded-2xl bg-white shadow-md">
+		<div class="h-[8rem] w-full text-center text-sm">
 			{#if navigating.to?.params?.barcode === product.code}
 				<span class="loading loading-ring loading-lg mx-auto my-auto"></span>
 			{:else if product.image_front_small_url}
 				<img
 					src={product.image_front_small_url ?? ''}
-					class="w-full h-full object-cover overflow-hidden rounded-t-2xl"
+					class="h-full w-full overflow-hidden rounded-t-2xl object-cover"
 					alt="Product front"
 				/>
 			{/if}
 		</div>
-		<div class="flex flex-col justify-center items-center font-semibold text-black px-2 pt-2">
-			<div class="font-bold text-center truncate w-[100%] 2xl:text-[18px]" title={product.product_name ? capitalizeWords(product.product_name) : product.code}>
+		<div class="flex flex-col items-center justify-center px-2 pt-2 font-semibold text-black">
+			<div
+				class="w-[100%] truncate text-center font-semibold 2xl:text-[16px]"
+				title={product.product_name ? capitalizeWords(product.product_name) : product.code}
+			>
 				{product.product_name ? capitalizeWords(product.product_name) : product.code}
 			</div>
-			<p class="text-xs">
+			<p class="text-xs text-gray-500">
 				{product.brands} - {product.quantity}
 			</p>
 		</div>
