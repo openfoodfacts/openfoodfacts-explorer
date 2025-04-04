04272423683fa3c5354f81d1fb00891fab94d887
abrahamvg <abrageor06@gmail.com>
2025-04-04 22:35:59 +0530

    fix: linting errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -49,8 +49,10 @@ export default [
 	{
 		rules: {
 			'@typescript-eslint/no-unused-vars': [
-				'error', { 
-					varsIgnorePattern: '^_$'
+				'error',
+				{
+					varsIgnorePattern: '^_$',
+					argsIgnorePattern: '^_$'
 				}
 			]
 		}
