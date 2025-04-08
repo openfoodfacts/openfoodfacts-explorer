e79ca6fdba931d4b56bb1f4a87e1b61d5b4d2646
Hao Sun <sun94456@ttu.edu>
2025-04-08 12:38:07 -0500

    feat: add a global nutrient calculator that calculates nutrition for a list of products (#336)
    Co-authored-by: VaiTon <eyadlorenzo@gmail.com>

--- src/routes/products/[barcode]/+page.svelte
+++ src/routes/products/[barcode]/+page.svelte
@@ -4,6 +4,7 @@
 	import { isConfigured as isFolksonomyConfigured } from '$lib/api/folksonomy';
 	import { preferences } from '$lib/settings';
 	import { navigating } from '$app/state';
+	import { addItemToCalculator, extractNutriments } from '$lib/stores/calculatorStore';
 
 	const TRACEABILITY_CODES_URL =
 		'https://wiki.openfoodfacts.org/Food_Traceability_Codes/EU_Food_establishments';
@@ -42,6 +43,16 @@
 	let product = $derived(data.state.product);
 
 	let lang = $derived($preferences.lang);
+
+	function addToCalculator() {
+		addItemToCalculator({
+			id: product.code,
+			name: product.product_name || product.code,
+			quantity: 100,
+			imageUrl: product.image_front_small_url,
+			nutriments: extractNutriments(product.nutriments)
+		});
+	}
 </script>
 
 <svelte:head>
@@ -64,6 +75,9 @@
 				See on OpenFoodFacts
 			</a>
 
+			<button class="btn btn-secondary max-sm:btn-sm mr-2" onclick={addToCalculator}>
+				Add to Calculator
+			</button>
 			<a
 				href={`/products/${product.code}/edit`}
 				class="btn btn-secondary max-sm:btn-sm"
