8a2a18bef40f976bfa6a6d3d02d07d3e0ba9fc9e
KarruHarin <harinkarru@gmail.com>
2025-04-08 23:51:16 +0530

    checks

--- src/routes/products/[barcode]/+page.svelte
+++ src/routes/products/[barcode]/+page.svelte
@@ -229,7 +229,13 @@
 			Open prices <span class="font-light italic">(alpha)</span>
 		</h1>
 
-		<Prices prices={data.prices.data} barcode={product.code} />
+		<Prices
+			prices={{
+				...data.prices.data,
+				items: data.prices.data.items.flat()
+			}}
+			barcode={product.code}
+		/>
 	</Card>
 {/if}
 
