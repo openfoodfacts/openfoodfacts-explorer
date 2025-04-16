357aa20be565b925cb5362b483c1dee555054573
swastikCommits <textswastik.alt@gmail.com>
2025-04-16 12:14:07 +0530

    added back the style tag

--- src/lib/ui/ImageModal.svelte
+++ src/lib/ui/ImageModal.svelte
@@ -130,3 +130,29 @@
 		</div>
 	</div>
 </dialog>
+
+
+<style>
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+</style>
