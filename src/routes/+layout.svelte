a6a5b5c899ba2c7ef6afe219b302e3a9166abb37
VaiTon <eyadlorenzo@gmail.com>
2025-04-18 03:11:46 +0200

    style: fix navbar overflow on lg breakpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,7 +52,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
 
-<div class="bg-base-100 navbar hidden px-10 lg:flex lg:px-40">
+<div class="bg-base-100 navbar hidden px-10 xl:flex">
 	<div class="navbar-start">
 		<a href="/"> <Logo /> </a>
 	</div>
@@ -107,7 +107,7 @@
 	</div>
 </div>
 
-<div class="bg-base-100 top-0 right-0 left-0 z-50 mx-4 lg:hidden">
+<div class="bg-base-100 top-0 right-0 left-0 z-50 mx-4 xl:hidden">
 	<div class="navbar bg-base-100 mx-auto mt-2 mb-2 max-w-7xl">
 		<div class="navbar-start">
 			<a href="/">
