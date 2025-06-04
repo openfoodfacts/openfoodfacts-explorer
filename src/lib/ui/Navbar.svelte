d9261c5440b36dd653c98aa898547310656a5a7a
heymitali <mitalisinghnaruka@gmail.com>
2025-06-05 00:01:29 +0530

    fixed the linting error

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -11,7 +11,7 @@
 <nav class="bg-secondary mt-2 mb-8 px-4">
 	<ul class="flex justify-center">
 		<div class="m-2 flex w-[70%] items-center justify-evenly 2xl:w-[60%]">
-			{#each navItems as item}
+			{#each navItems as item (item.name)}
 				<li>
 					<a href={item.href} class="text-secondary-content font-medium hover:underline"
 						>{item.name}</a
