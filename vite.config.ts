7752db18f82c831a4d58b4a1d908c970c54511da
heymitali <mitalisinghnaruka@gmail.com>
2025-06-07 20:19:59 +0530

    copies web component assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { version as packageVersion } from './package.json';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'node_modules/@openfoodfacts/openfoodfacts-webcomponents/dist/assets/**/*',
+					dest: 'assets'
+				}
+			]
+		})
+	],
 	define: {
 		'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageVersion)
 	}
