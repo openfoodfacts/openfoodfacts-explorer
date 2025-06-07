f60887879f5e45c46cb979bd6c222df6ac3d6c4a
heymitali <mitalisinghnaruka@gmail.com>
2025-06-07 18:59:26 +0530

    feat: static pages using iframes

---
+++ src/lib/ui/StaticPageIframe.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	type Props = { src: string; title: string };
+	let { src, title }: Props = $props();
+
+	let frameHeight = $state('100vh'); // Default height, can be adjusted
+
+	onMount(() => {
+		const abortController = new AbortController();
+		window.addEventListener(
+			'message',
+			(event: { data: { frameHeight?: number } }) => {
+				if (!event.data.frameHeight) return;
+
+				console.debug('Received frameHeight:', event.data.frameHeight);
+				frameHeight = event.data.frameHeight + 'px';
+			},
+			{ signal: abortController.signal }
+		);
+
+		return () => {
+			abortController.abort(); // Cleanup the event listener on component unmount
+		};
+	});
+</script>
+
+<iframe
+	{src}
+	class="w-full border-0"
+	{title}
+	scrolling="no"
+	frameborder="0"
+	style="height: {frameHeight};"
+></iframe>
