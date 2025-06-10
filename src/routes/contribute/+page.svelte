737a28d826d7be486eab139fa7296a74109d519f
heymitali <mitalisinghnaruka@gmail.com>
2025-06-10 22:58:55 +0530

    added custom heights

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -1,7 +1,7 @@
 <iframe
-  src="https://world.openfoodfacts.org/contribute?content_only=1"
-  class="w-full border-0 xl:h-[2400px] lg:h-[2800px] md:h-[3300px] h-[4800px]"
-  title="Contribute to Open Food Facts"
-  scrolling="no"
-  frameborder="0"
+	src="https://world.openfoodfacts.org/contribute?content_only=1"
+	class="h-[4800px] w-full border-0 md:h-[3300px] lg:h-[2800px] xl:h-[2400px]"
+	title="Contribute to Open Food Facts"
+	scrolling="no"
+	frameborder="0"
 ></iframe>
--- src/routes/discover/+page.svelte
+++ src/routes/discover/+page.svelte
@@ -1,12 +1,10 @@
-
 <iframe
-  src="https://world.openfoodfacts.org/discover?content_only=1"
-  class="w-full border-0 xl:h-[2400px] lg:h-[2800px] md:h-[2900px] sm:h-[4800px] h-[5200px]"
-  title="Discover Open Food Facts"
-  scrolling="no"
-  frameborder="0"
+	src="https://world.openfoodfacts.org/discover?content_only=1"
+	class="h-[5200px] w-full border-0 sm:h-[4800px] md:h-[2900px] lg:h-[2800px] xl:h-[2400px]"
+	title="Discover Open Food Facts"
+	scrolling="no"
+	frameborder="0"
 ></iframe>
-
 
 <!-- 
 <iframe
