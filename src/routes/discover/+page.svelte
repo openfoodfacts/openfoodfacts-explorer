d74a79a2e89fb91ba05e28ad1473e4c7fd736191
heymitali <mitalisinghnaruka@gmail.com>
2025-06-10 23:49:20 +0530

    removed commented code

--- src/routes/discover/+page.svelte
+++ src/routes/discover/+page.svelte
@@ -5,13 +5,3 @@
 	scrolling="no"
 	frameborder="0"
 ></iframe>
-
-<!-- 
-<iframe
-  src="https://world.openfoodfacts.org/discover?content_only=1"
-  class="w-full border-0"
-  style="height: 1200px;"
-  title="Discover Open Food Facts"
-  scrolling="no"
-  frameborder="0"
-></iframe> -->
--- src/routes/producers/+page.svelte
+++ src/routes/producers/+page.svelte
@@ -1,9 +1,7 @@
 <iframe
 	src="https://world.openfoodfacts.org/producers?content_only=1"
-	class="h-[5300px] w-full border-0"
+	class="h-[810rem] md:h-[670rem] lg:h-[590rem] xl:h-[570rem] w-full border-0"
 	title="Contribute to Open Food Facts"
 	scrolling="no"
 	frameborder="0"
 ></iframe>
-
-<!-- class="w-full border-0 xl:h-[5000px] lg:h-[2800px] md:h-[3300px] h-[4800px]" -->
