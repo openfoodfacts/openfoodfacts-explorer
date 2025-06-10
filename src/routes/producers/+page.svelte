0a351bb6b80048a666910d1389af8209ea0431e0
heymitali <mitalisinghnaruka@gmail.com>
2025-06-11 00:22:32 +0530

    responsiveness fixes

--- src/routes/producers/+page.svelte
+++ src/routes/producers/+page.svelte
@@ -1,6 +1,6 @@
 <iframe
 	src="https://world.openfoodfacts.org/producers?content_only=1"
-	class="h-[810rem] w-full border-0 md:h-[670rem] lg:h-[590rem] xl:h-[570rem]"
+	class="h-[700rem] w-full border-0 md:h-[580rem] lg:h-[520rem] xl:h-[480rem]"
 	title="Contribute to Open Food Facts"
 	scrolling="no"
 	frameborder="0"
