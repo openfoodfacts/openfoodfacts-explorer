f220fa216d797b6e6a738e1454c389969bc9a671
heymitali <mitalisinghnaruka@gmail.com>
2025-06-10 23:49:30 +0530

    prettier

--- src/routes/producers/+page.svelte
+++ src/routes/producers/+page.svelte
@@ -1,6 +1,6 @@
 <iframe
 	src="https://world.openfoodfacts.org/producers?content_only=1"
-	class="h-[810rem] md:h-[670rem] lg:h-[590rem] xl:h-[570rem] w-full border-0"
+	class="h-[810rem] w-full border-0 md:h-[670rem] lg:h-[590rem] xl:h-[570rem]"
 	title="Contribute to Open Food Facts"
 	scrolling="no"
 	frameborder="0"
