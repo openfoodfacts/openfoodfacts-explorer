84c8879c5d9d1fe74935b2f924534af9f960066c
heymitali <mitalisinghnaruka@gmail.com>
2025-06-17 04:09:13 +0530

    basic auth flow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,6 +49,83 @@
 
 	let searchActive = $state(false);
 	let accordionOpen = $state(false);
+
+	// TODO: update this propertly
+	let userLoginState = false;
+
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	const keycloak = 'http://auth.openfoodfacts.localhost:5600/realms/open-products-facts';
+	const pkceClientId = 'OFF_EXP';
+
+
+	function base64URLEncode(bytes: any) {
+		const b64 = btoa(String.fromCodePoint(...bytes));
+		const encoded = b64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+		return encoded;
+	}
+
+	async function pkceLogin() {
+		const verifier = base64URLEncode(crypto.getRandomValues(new Uint8Array(32)));
+		// Save the verifier so that the code page can access it
+		// Should really use sessionStorage but for tests when we click a verification email that starts a separate session
+		localStorage.setItem('verifier', verifier);
+		const codeChallenge = base64URLEncode(
+			new Uint8Array(
+				await crypto.subtle.digest({ name: 'SHA-256' }, new TextEncoder().encode(verifier))
+			)
+		);
+
+		const nonce = crypto.getRandomValues(new BigUint64Array(1))[0];
+		const lang = "en";
+		const clientSecret = '1';
+
+		const redirectUri = `http://localhost:5604/${page}.html?clientId=${clientId}&pkceClientId=${pkceClientId}&clientSecret=${clientSecret}&lang=${lang}&keycloak=${encodeURIComponent(keycloak)}&mode=pkce`;
+
+		const url = `${keycloak}/protocol/openid-connect/auth?response_type=code&client_id=${pkceClientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=openid+profile+offline_access&state=${nonce}&ui_locales=${lang}&code_challenge=${codeChallenge}&code_challenge_method=S256`;
+
+		userLoginState = true; // Set user login state to true
+		window.location.href = url;
+	}
+
+	async function getAccessToken() {
+		const verifier = localStorage.getItem("verifier");
+		const body = new URLSearchParams({
+			code: "code", // figure this out
+			grant_type: "authorization_code",
+			redirect_uri: "http://localhost:5173/accesstoken.html",
+			client_id: pkceClientId,
+			code_verifier: verifier ?? ''
+		});
+		const response = await fetch(`${keycloak}/protocol/openid-connect/token`, {
+			method: "POST",
+			body: body,
+			headers: new Headers(),
+		});
+		const jwt = await response.json();
+		const accessToken = JSON.parse(atob(jwt.access_token.split(".")[1]));
+		return {
+			accessToken: jwt.access_token,
+			idToken: jwt.id_token,
+			expiresIn: jwt.expires_in,
+			accessTokenPayload: accessToken
+		};
+	}
+
+	async function logoutUser() {
+		const { idToken } = await getAccessToken();
+		const redirectUri = `http://localhost:5604/logout.html`;
+		userLoginState = false; // Set user login state to false
+
+		window.location.href = `${keycloak}/protocol/openid-connect/logout?client_id=${pkceClientId}&post_logout_redirect_uri=${encodeURIComponent(redirectUri)}&id_token_hint=${idToken}`;
+	}
+
+	function getAccountUrl() {
+		return `${keycloak}/account/#/`;
+	}
+	
+	// const authUrl = `http://auth.openfoodfacts.localhost:5600/realms/open-products-facts/protocol/openid-connect/auth?client_id=${clientId}&redirect_uri=${redirect_uri}&state=de8df7a8-a591-4f38-908d-d7340ce7e04e&response_mode=query&response_type=code&scope=openid&nonce=20bf70cc-6d3a-48df-ab62-6dd053cfa582&code_challenge=${codeChallenge}&code_challenge_method=S256`;
+
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 </script>
 
 <svelte:head>
@@ -114,6 +191,18 @@
 				>
 					<span class="icon-[mdi--github] h-8 w-8"></span>
 				</a>
+				{#if userLoginState}
+					<a class="btn btn-outline link" href={getAccountUrl()}>Account</a>
+					<button
+						class="btn btn-outline link logout-btn"
+						onclick={logoutUser}>Log out</button
+					>
+				{:else}
+					<!-- svelte-ignore a11y_missing_attribute -->
+					<a class="btn btn-outline link">
+						<button onclick={pkceLogin}> Login </button>
+					</a>
+				{/if}
 			</div>
 		</div>
 	</div>
