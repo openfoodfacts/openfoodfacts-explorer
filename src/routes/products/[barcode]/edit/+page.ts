877f7ab82638382bcc323ad0cd721e66cba431f7
VaiTon <eyadlorenzo@gmail.com>
2025-08-20 22:21:51 +0200

    chore: use more SDK

--- src/routes/products/[barcode]/edit/+page.ts
+++ src/routes/products/[barcode]/edit/+page.ts
@@ -2,24 +2,25 @@ import { error } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 
 import {
-	getProduct,
 	getTaxo,
 	type Category,
 	type Origin,
 	type Label,
 	type Brand,
 	type Store,
-	type Country
+	type Country,
+	createProductsApi
 } from '$lib/api';
 import { userInfo } from '$lib/stores/pkceLoginStore';
 import { PRODUCT_STATUS } from '$lib/const';
 
 import type { PageLoad } from './$types';
 import { dev } from '$app/environment';
+import { preferences } from '$lib/settings';
 
 export const ssr = false;
 
-export const load = (async ({ fetch, params }) => {
+export const load: PageLoad = async ({ fetch, params }) => {
 	if (window == null) {
 		error(500, 'This page requires a browser environment');
 	}
@@ -38,8 +39,14 @@ export const load = (async ({ fetch, params }) => {
 		});
 	}
 
-	const [product, categories, labels, brands, stores, origins, countries] = await Promise.all([
-		getProduct(params.barcode, fetch),
+	const off = createProductsApi(fetch);
+
+	const [productState, categories, labels, brands, stores, origins, countries] = await Promise.all([
+		off.getProductV3(params.barcode, {
+			// @ts-expect-error - TODO: To be fixed in the SDK
+			lc: get(preferences).lang,
+			cc: get(preferences).country
+		}),
 		getTaxo<Category>('categories', fetch),
 		getTaxo<Label>('labels', fetch),
 		getTaxo<Brand>('brands', fetch),
@@ -48,12 +55,20 @@ export const load = (async ({ fetch, params }) => {
 		getTaxo<Country>('countries', fetch)
 	]);
 
-	if (product.status === 'failure' && product.result.id === 'product_not_found') {
+	if (productState == null) {
+		error(404, 'Product not found');
+	}
+
+	// @ts-expect-error - https://github.com/openfoodfacts/openfoodfacts-server/issues/12273
+	console.debug(`Product state for barcode ${params.barcode}:`, productState.status);
+
+	// @ts-expect-error - https://github.com/openfoodfacts/openfoodfacts-server/issues/12273
+	if (productState.status === 'failure' && productState.result?.id === 'product_not_found') {
 		return {
 			state: {
 				status: PRODUCT_STATUS.EMPTY,
 				product: null,
-				errors: product.errors
+				errors: productState.errors
 			},
 			categories,
 			labels,
@@ -62,15 +77,17 @@ export const load = (async ({ fetch, params }) => {
 			origins,
 			countries
 		};
-	} else if (product.status === 'failure') {
+	}
+	// @ts-expect-error - https://github.com/openfoodfacts/openfoodfacts-server/issues/12273
+	else if (productState.status === 'failure') {
 		error(404, {
 			message: 'Failure to load product',
-			errors: product.errors
+			errors: productState.errors
 		});
 	}
 
 	return {
-		state: product,
+		state: productState,
 		categories,
 		labels,
 		brands,
@@ -78,4 +95,4 @@ export const load = (async ({ fetch, params }) => {
 		origins,
 		countries
 	};
-}) satisfies PageLoad;
+};
