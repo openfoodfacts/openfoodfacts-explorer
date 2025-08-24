1b3160a4111cf0437dc4a337c5203576af8d88e9
VaiTon <eyadlorenzo@gmail.com>
2025-08-24 19:01:38 +0200

    refactor: update OpenFoodFacts SDK and refactor API usage

--- src/lib/api/prices.ts
+++ src/lib/api/prices.ts
@@ -1,10 +1,8 @@
 import { get } from 'svelte/store';
 import { preferences } from '$lib/settings';
-import { PricesApi, type Prices } from '@openfoodfacts/openfoodfacts-nodejs';
+import { PricesApi } from '@openfoodfacts/openfoodfacts-nodejs';
 
 const BASE_URL = import.meta.env.VITE_PRICES_API_URL;
-
-export type PriceFull = Prices['items'][number][number];
 
 export function isConfigured() {
 	return BASE_URL != null;
