1b31e814e234b92f4f202cc4dcedc4e01a337a3d
VaiTon <eyadlorenzo@gmail.com>
2025-09-01 20:25:04 +0200

    feat: implement wrapFetchWithCredentials utility for authenticated fetch requests

--- src/lib/api/utils.ts
+++ src/lib/api/utils.ts
@@ -7,3 +7,21 @@ export function formBody(params: Record<string, string | null | undefined>) {
 	}
 	return formBody;
 }
+
+export function wrapFetchWithCredentials(
+	fetch: typeof globalThis.fetch,
+	url: URL
+): { fetch: typeof globalThis.fetch; url: URL } {
+	if (url.username && url.password) {
+		fetch = async (input, init) => {
+			const headers = new Headers(init?.headers);
+			headers.set('Authorization', 'Basic ' + btoa(url.username + ':' + url.password));
+			return fetch(input, { ...init, headers });
+		};
+	}
+
+	url.username = '';
+	url.password = '';
+
+	return { fetch, url };
+}
