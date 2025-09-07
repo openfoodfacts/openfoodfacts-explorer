7f7f5b5ade9a8172db971f8eeaf6c4124edba0c5
VaiTon <eyadlorenzo@gmail.com>
2025-09-07 17:28:33 +0200

    fix: Make PriceFull an alias to use the SDK

--- src/lib/api/prices.ts
+++ src/lib/api/prices.ts
@@ -1,20 +1,10 @@
 import { get } from 'svelte/store';
 import { preferences } from '$lib/settings';
-import { PricesApi } from '@openfoodfacts/openfoodfacts-nodejs';
+import { PricesApi, type PaginatedPriceFullList } from '@openfoodfacts/openfoodfacts-nodejs';
 
 const BASE_URL = import.meta.env.VITE_PRICES_API_URL;
 
-// TODO: Replace with the one from the SDK
-export type PriceFull = {
-	price: number;
-	location: {
-		osm_lat?: number;
-		osm_lon?: number;
-		osm_display_name?: string;
-		osm_address_city?: string;
-		osm_address_country?: string;
-	};
-};
+export type PriceFull = PaginatedPriceFullList['items'][number][number];
 
 export function isConfigured() {
 	return BASE_URL != null;
