1311e904dae7ec8e3328306fcbf52e0e33fcec76
VaiTon <eyadlorenzo@gmail.com>
2025-09-21 18:24:14 +0200

    feat: Add user profile statistics for created and edited products

--- src/routes/users/[user]/+page.ts
+++ src/routes/users/[user]/+page.ts
@@ -1,18 +1,26 @@
 import type { Product } from '@openfoodfacts/openfoodfacts-nodejs';
 import type { PageLoad } from './$types';
 
+// TODO: remove this
+type FacetResponse = {
+	count: number;
+	products: Product[];
+};
+
 export const load: PageLoad = async ({ fetch, params }) => {
 	const { user } = params;
 
 	const contributorResponse = fetch(
 		`https://world.openfoodfacts.org/facets/contributors/${user}.json`
-	).then((it) => it.json()) as Promise<{ products: Product[] }>;
+	).then((it) => it.json()) as Promise<FacetResponse>;
 
 	const editorResponse = fetch(`https://world.openfoodfacts.org/facets/editors/${user}.json`).then(
 		(it) => it.json()
-	) as Promise<{ products: Product[] }>;
+	) as Promise<FacetResponse>;
 
 	const [contributorData, editorData] = await Promise.all([contributorResponse, editorResponse]);
+
+	console.debug('contributorData', contributorData);
 
 	return {
 		user,
