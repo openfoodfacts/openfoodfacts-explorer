56864b7b8602750aee8928cc0428fe0cdc065404
VaiTon <eyadlorenzo@gmail.com>
2025-09-23 02:27:55 +0200

    feat(homepage): real product and editor count

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,26 @@
+import { wrapFetchWithCredentials } from '$lib/api/utils';
+import { API_HOST } from '$lib/const';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async () => {};
+async function getNumberOfProducts(fetch: typeof window.fetch): Promise<number> {
+	const { fetch: wrappedFetch, url } = wrapFetchWithCredentials(fetch, new URL(API_HOST));
+	const res = await wrappedFetch(`${url}.json`);
+	const data = await res.json();
+	return data?.count || 0;
+}
+
+async function getNumberOfEditors(fetch: typeof window.fetch): Promise<number> {
+	const { fetch: wrappedFetch, url } = wrapFetchWithCredentials(fetch, new URL(API_HOST));
+	const res = await wrappedFetch(`${url}facets/contributors.json`);
+	const data = await res.json();
+	return data?.count || 0;
+}
+
+export const load: PageLoad = async ({ fetch }) => {
+	const productCount = getNumberOfProducts(fetch);
+	const editorCount = getNumberOfEditors(fetch);
+	return {
+		productCount: await productCount,
+		editorCount: await editorCount
+	};
+};
