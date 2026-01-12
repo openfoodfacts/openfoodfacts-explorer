c995578e073c7c8cbb9415967a68483e23f916e9
VaiTon <eyadlorenzo@gmail.com>
2026-01-12 20:30:51 +0100

    refactor: replace custom API call with sdk in getUserLogin

--- src/routes/settings/+page.ts
+++ src/routes/settings/+page.ts
@@ -1,33 +1,17 @@
-import { createProductsApi, getTaxo } from '$lib/api';
 import { type Country, type Language } from '@openfoodfacts/openfoodfacts-nodejs';
-import type { PageLoad } from './$types';
-import { createPricesApi } from '$lib/api/prices';
-import { wrapFetchWithCredentials } from '$lib/api/utils';
-import { API_HOST } from '$lib/const';
+
 import { dev } from '$app/environment';
+import { createProductsApi, getTaxo } from '$lib/api';
+import { createPricesApi } from '$lib/api/prices';
+
+import type { PageLoad } from './$types';
 
 export const ssr = false;
 
 async function getUserLogin(fetch: typeof window.fetch) {
-	const { fetch: authenticatedFetch, url } = wrapFetchWithCredentials(fetch, new URL(API_HOST));
-
-	// TODO: replace this with SDK
 	try {
-		const response = await authenticatedFetch(`${url}cgi/auth.pl?body=1`);
-		if (!response.ok) {
-			return null;
-		}
-
-		return (await response.json()) as {
-			status: 1 | 0;
-			status_verbose: string;
-			user: {
-				admin: 0 | 1;
-				moderator: 1 | 0;
-				name: string;
-			};
-			user_id: string;
-		};
+		const offApi = createProductsApi(fetch);
+		return offApi.getLoginStatus();
 	} catch {
 		console.warn('Could not reach the API, using dummy user in dev mode');
 		if (dev) {
@@ -47,8 +31,9 @@ export const load: PageLoad = async ({ fetch }) => {
 	const languages = await getTaxo<Language>('languages', fetch);
 	let countries = await getTaxo<Country>('countries', fetch);
 
-	// FIX: not every country has a language_code_2, so we need to filter them out
+	// Not every country has a language_code_2, so we need to filter them out
 	// as they are used to store the language preference and to query the API
+	// TODO: fix the taxonomy to add missing codes
 	countries = Object.fromEntries(
 		Object.entries(countries).filter(([, country]) => country.country_code_2 != null)
 	);
