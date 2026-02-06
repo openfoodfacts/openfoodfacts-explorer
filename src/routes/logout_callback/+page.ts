53ead8064473af1bc5c13dcd42e080cb4cd4a0ad
VaiTon <eyadlorenzo@gmail.com>
2026-02-06 19:54:21 +0100

    feat(auth): show progress on logout callback

--- src/routes/logout_callback/+page.ts
+++ src/routes/logout_callback/+page.ts
@@ -1,11 +1,1 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
-
-import { clearAuthTokens } from '$lib/stores/pkceLoginStore';
-
 export const ssr = false;
-
-export const load: PageLoad = () => {
-	clearAuthTokens();
-	throw redirect(302, '/');
-};
