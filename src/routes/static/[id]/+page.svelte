60c64fa1ebed1565bd989a933aa7948dfe89ca0f
Nagam Surya Narayana <nagamsuryanarayana01@gmail.com>
2026-02-24 14:56:14 +0530

    Add locale-aware loading for static pages

--- src/routes/static/[id]/+page.svelte
+++ src/routes/static/[id]/+page.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
 	import StaticPageIframe from '$lib/ui/StaticPageIframe.svelte';
 	import { page } from '$app/state';
+	import { locale } from 'svelte-i18n';
+	import { onDestroy } from 'svelte';
 
-	let iframeUrl = $derived(`https://world.openfoodfacts.org/${page.params.id}?content_only=1`);
+	let currentLocale = 'en';
+	let loading = true;
+	let error = false;
+
+	const unsubscribe = locale.subscribe((val) => {
+		if (val) {
+			currentLocale = val.split('-')[0];
+		}
+	});
+
+	onDestroy(() => {
+		unsubscribe();
+	});
+
+	// Locale-aware URL
+	$: iframeUrl = `https://${currentLocale}.openfoodfacts.org/${page.params.id}?content_only=1`;
+
+	function handleLoad() {
+		loading = false;
+		error = false;
+	}
+
+	function handleError() {
+		loading = false;
+		error = true;
+	}
 </script>
-
-<StaticPageIframe src={iframeUrl} />
