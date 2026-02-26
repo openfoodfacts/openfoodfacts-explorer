8f6ba9fec83fe718bc6dc700ca3400d900179ddd
Nagam Surya Narayana <nagamsuryanarayana01@gmail.com>
2026-02-27 00:17:11 +0530

    refactor: cleanup static page implementation

--- src/routes/static/[id]/+page.svelte
+++ src/routes/static/[id]/+page.svelte
@@ -2,19 +2,17 @@
 	import StaticPageIframe from '$lib/ui/StaticPageIframe.svelte';
 	import { page } from '$app/state';
 	import { locale } from 'svelte-i18n';
-	import { get } from 'svelte/store';
 
 	let loading = $state(true);
 	let error = $state(false);
 
-	// Use get() safely for initial value
-	let currentLocale = $state('en');
-
-	$effect(() => {
-		const val = get(locale);
-		currentLocale = val ? val.split('-')[0] : 'en';
+	// Read locale using Svelte 5 rune
+	const currentLocale = $derived(() => {
+		const val = $locale;
+		return val ? val.split('-')[0] : 'en';
 	});
 
+	// Reactive iframe URL
 	const iframeUrl = $derived(() => {
 		return `https://${currentLocale}.openfoodfacts.org/${page.params.id}?content_only=1`;
 	});
