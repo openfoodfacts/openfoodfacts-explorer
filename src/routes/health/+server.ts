e6fe9b451ee404bd8f359adc79983c47a0328acc
Saish Mungase <saishmungase@gmail.com>
2026-03-12 19:28:45 +0530

    feat: update /health endpoint to meet Docker acceptance criteria

--- src/routes/health/+server.ts
+++ src/routes/health/+server.ts
@@ -1,9 +1,13 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import pkg from '../../../package.json';
 
 export const GET: RequestHandler = () => {
 	return json(
-		{ status: 'up' },
+		{
+			status: 'ok',
+			version: pkg.version
+		},
 		{
 			status: 200,
 			headers: { 'Cache-Control': 'no-cache' }
