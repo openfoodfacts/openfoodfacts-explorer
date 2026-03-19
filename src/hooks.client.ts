68bbeb19e08c155a0dc21e9580caeee4d1fbfa98
Vidhushaaa30 <vidhushanagarajan30@gmail.com>
2026-03-19 18:30:53 +0530

    fix: reduce sentry tracing sample rate

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -4,7 +4,7 @@ import * as Sentry from '@sentry/sveltekit';
 Sentry.init({
 	dsn: 'https://6a3e1451763425bd80a12c9548969f22@o241488.ingest.us.sentry.io/4509950733320194',
 
-	tracesSampleRate: 1.0
+	tracesSampleRate: process.env.NODE_ENV === 'development' ? 1.0 : 0.1
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
