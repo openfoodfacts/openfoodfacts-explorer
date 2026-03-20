b71c85c301a8048202df36d876ac32a24b910bf9
omar <202027471@std.sci.cu.edu.eg>
2026-03-20 10:24:24 +0200

    fix: correct Robotoff insights API endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,7 +13,8 @@ export * from './api/knowledgepanels';
 
 export function createRobotoffApi(fetch: typeof window.fetch) {
 	const { fetch: wrappedFetch, url } = wrapFetchWithCredentials(fetch, new URL(ROBOTOFF_URL));
-	return new Robotoff(wrappedFetch, { baseUrl: url.toString() });
+	const baseUrl = new URL('/api/v1', url).toString();
+	return new Robotoff(wrappedFetch, { baseUrl });
 }
 
 export function createKeycloakApi(fetch: typeof window.fetch, url: URL) {
