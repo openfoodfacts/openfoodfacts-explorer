6b5777546acf1ba4d8cca797a5c82210d84f2e21
Vidhushaaa30 <vidhushanagarajan30@gmail.com>
2026-03-20 12:07:27 +0530

    fix: move sentry dsn to environment variable

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,8 +2,7 @@ import { handleErrorWithSentry } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-	dsn: 'https://6a3e1451763425bd80a12c9548969f22@o241488.ingest.us.sentry.io/4509950733320194',
-
+  dsn: import.meta.env.PUBLIC_SENTRY_DSN,
 	tracesSampleRate: 1.0
 });
 
