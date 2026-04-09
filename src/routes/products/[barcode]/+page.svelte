314a94115bee66138efe4b85c185200fe370993b
devsk18 <kthampansam18@gmail.com>
2026-04-09 16:13:34 +0530

    fix: move shortcut registrations into onMount

--- src/routes/products/[barcode]/+page.svelte
+++ src/routes/products/[barcode]/+page.svelte
@@ -26,7 +26,7 @@
 	import { OpenFoodFacts, type Product } from '@openfoodfacts/openfoodfacts-nodejs';
 	import type { KnowledgePanels } from '$lib/api/knowledgepanels';
 	import NutritionCalculator from '$lib/ui/NutritionCalculator.svelte';
-	import { onDestroy } from 'svelte';
+	import { onMount } from 'svelte';
 	import { getShortcutCtx } from '$lib/stores/shortcuts';
 	import type { ProductGroupedAttributes } from './types';
 	import { personalizedSearch } from '$lib/stores/preferencesStore';
@@ -66,43 +66,46 @@
 	let showBarcode = $state(false);
 
 	const shortcutCtx = getShortcutCtx();
-	shortcutCtx.set('Shift+B', {
-		description: $_('product.shortcuts.show_barcode'),
-		action: () => (showBarcode = !showBarcode)
-	});
 
-	shortcutCtx.set('A', {
-		description: $_('product.shortcuts.api_page'),
-		action: () => {
-			if (product.code) {
-				window.open(PRODUCT_URL(product.code), '_blank');
+	onMount(() => {
+		shortcutCtx.set('Shift+B', {
+			description: $_('product.shortcuts.show_barcode'),
+			action: () => (showBarcode = !showBarcode)
+		});
+
+		shortcutCtx.set('A', {
+			description: $_('product.shortcuts.api_page'),
+			action: () => {
+				if (product.code) {
+					window.open(PRODUCT_URL(product.code), '_blank');
+				}
 			}
-		}
-	});
+		});
 
-	shortcutCtx.set('E', {
-		description: $_('product.shortcuts.edit_same_window'),
-		action: () => {
-			if (product.code) {
-				goto(resolve('/products/[barcode]/edit', { barcode: product.code }));
+		shortcutCtx.set('E', {
+			description: $_('product.shortcuts.edit_same_window'),
+			action: () => {
+				if (product.code) {
+					goto(resolve('/products/[barcode]/edit', { barcode: product.code }));
+				}
 			}
-		}
-	});
+		});
 
-	shortcutCtx.set('Shift+E', {
-		description: $_('product.shortcuts.edit_new_window'),
-		action: () => {
-			if (product.code) {
-				window.open(resolve('/products/[barcode]/edit', { barcode: product.code }), '_blank');
+		shortcutCtx.set('Shift+E', {
+			description: $_('product.shortcuts.edit_new_window'),
+			action: () => {
+				if (product.code) {
+					window.open(resolve('/products/[barcode]/edit', { barcode: product.code }), '_blank');
+				}
 			}
-		}
-	});
+		});
 
-	onDestroy(() => {
-		shortcutCtx.delete('A');
-		shortcutCtx.delete('E');
-		shortcutCtx.delete('Shift+E');
-		shortcutCtx.delete('Shift+B');
+		return () => {
+			shortcutCtx.delete('A');
+			shortcutCtx.delete('E');
+			shortcutCtx.delete('Shift+E');
+			shortcutCtx.delete('Shift+B');
+		};
 	});
 
 	async function getProductAttributes(code: string): Promise<ProductGroupedAttributes[]> {
