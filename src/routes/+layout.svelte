c7b44c693d93c299ac9a754cbf0211fb1b1a52f4
VaiTon <eyadlorenzo@gmail.com>
2026-05-05 16:36:38 +0200

    chore: Replace @sinnwerkstatt/sveltekit-matomo with local matomo import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-	import { Matomo } from '@sinnwerkstatt/sveltekit-matomo';
+	import { Matomo } from '$lib/matomo';
 
 	import '../app.css';
 	import 'leaflet/dist/leaflet.css';
