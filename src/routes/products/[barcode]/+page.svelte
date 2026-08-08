3805c81b3c2f16b9dd618f4c561f6eda8cceb550
VaiTon <eyadlorenzo@gmail.com>
2026-08-08 22:14:02 +0200

    feat: add Open X Facts flavor landing links

--- src/routes/products/[barcode]/+page.svelte
+++ src/routes/products/[barcode]/+page.svelte
@@ -33,6 +33,7 @@
 	import type { ProductGroupedAttributes } from './types';
 	import { personalizedSearch } from '$lib/stores/preferencesStore';
 	import { PRODUCT_URL } from '$lib/const';
+	import { toWebsiteFlavor } from '$lib/flavor';
 	import { resolve } from '$app/paths';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
@@ -63,7 +64,12 @@
 	let websiteCtx = getWebsiteCtx();
 	$effect(() => {
 		// Update website context based on product type
-		if (product.product_type) websiteCtx.flavor = product.product_type;
+		if ($websiteCtx.forcedFlavor == null && product.product_type) {
+			const productFlavor = toWebsiteFlavor(product.product_type);
+			if ($websiteCtx.flavor !== productFlavor) {
+				websiteCtx.update((ctx) => ({ ...ctx, flavor: productFlavor }));
+			}
+		}
 	});
 
 	// Track product score presence (fire once per product page view)
